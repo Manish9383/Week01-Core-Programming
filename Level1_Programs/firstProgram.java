d8bd1c6dd3300767c958fad9f01155bf57f6cb71
Manish <patelmanish131149198@gmail.com>
2025-01-11 10:28:42 +0530

    Question_1

---
+++ Level1_Programs/firstProgram.java
@@ -0,0 +1,9 @@
+
+public class firstProgram{
+	public static void main(String args[]){
+	int currentAge =2024;
+	int harryDOB=2000;
+	int age=currentAge-harryDOB;//hellow
+	System.out.println("Harry's age in 2024 is "+ age);
+	}
+}
