30b06bd68e659d53de03c9e2d5934e80b266f5ff
Manish <patelmanish131149198@gmail.com>
2025-01-13 10:50:41 +0530

    Pushing code9

---
+++ Level3_Programs/SecondProgram.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+
+public class SecondProgram {
+
+    // Method to find the count of digits in the number
+    public static int countDigits(int number) {
+        return String.valueOf(number).length();
+    }
+
+    // Method to store the digits of the number in a digits array
+    public static int[] storeDigits(int number) {
+        int numDigits = countDigits(number);
+        int[] digits = new int[numDigits];
+        for (int i = numDigits - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    // Method to check if the number is a Duck number
+    public static boolean isDuckNumber(int number) {
+        int[] digits = storeDigits(number);
+        for (int digit : digits) {
+            if (digit != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Method to check if the number is an Armstrong number
+    public static boolean isArmstrongNumber(int number) {
+        int[] digits = storeDigits(number);
+        int numDigits = digits.length;
+        int sum = 0;
+        int originalNumber = number;
+        for (int digit : digits) {
+            sum += Math.pow(digit, numDigits);
+        }
+        return sum == originalNumber;
+    }
+
+    // Method to find the largest and second-largest elements in the digits array
+    public static int[] findLargestAndSecondLargest(int[] digits) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int digit : digits) {
+            if (digit > largest) {
+                secondLargest = largest;
+                largest = digit;
+            } else if (digit > secondLargest && digit != largest) {
+                secondLargest = digit;
+            }
+        }
+        return new int[]{largest, secondLargest};
+    }
+
+    // Method to find the smallest and second-smallest elements in the digits array
+    public static int[] findSmallestAndSecondSmallest(int[] digits) {
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int digit : digits) {
+            if (digit < smallest) {
+                secondSmallest = smallest;
+                smallest = digit;
+            } else if (digit < secondSmallest && digit != smallest) {
+                secondSmallest = digit;
+            }
+        }
+        return new int[]{smallest, secondSmallest};
+    }
+
+    public static void main(String[] args) {
+        // Input number
+        int number = 153; // Example number
+
+        // Count of digits
+        int count = countDigits(number);
+        System.out.println("Count of digits in " + number + ": " + count);
+
+        // Store the digits in an array
+        int[] digits = storeDigits(number);
+        System.out.println("Digits of " + number + ": " + Arrays.toString(digits));
+
+        // Check if the number is a Duck number
+        if (isDuckNumber(number)) {
+            System.out.println(number + " is a Duck number.");
+        } else {
+            System.out.println(number + " is not a Duck number.");
+        }
+
+        // Check if the number is an Armstrong number
+        if (isArmstrongNumber(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+
+        // Find the largest and second-largest digits
+        int[] largestAndSecondLargest = findLargestAndSecondLargest(digits);
+        System.out.println("Largest digit: " + largestAndSecondLargest[0]);
+        System.out.println("Second largest digit: " + largestAndSecondLargest[1]);
+
+        // Find the smallest and second-smallest digits
+        int[] smallestAndSecondSmallest = findSmallestAndSecondSmallest(digits);
+        System.out.println("Smallest digit: " + smallestAndSecondSmallest[0]);
+        System.out.println("Second smallest digit: " + smallestAndSecondSmallest[1]);
+    }
+}
