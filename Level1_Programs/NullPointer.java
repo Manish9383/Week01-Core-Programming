4dc244ba663609cd7f18adabfcdd5535427de378
Manish <patelmanish131149198@gmail.com>
2025-01-15 23:07:23 +0530

    Question_05

---
+++ Level1_Programs/NullPointer.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class NullPointer {
+
+    public static void generateException(){
+		try{
+			String text=null;
+			
+			text.length();
+			
+		}
+		catch(NullPointerException e){
+			handleException(e);
+		}
+	}
+	
+	public static void handleException(Exception e){
+		System.out.println("Exception Occured : "+e.getMessage());
+	}
+
+    public static void main(String[] args) {
+		
+		generateException();
+		
+		
+    }
+}
