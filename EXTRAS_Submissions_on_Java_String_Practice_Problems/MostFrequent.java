f232dbb396ba252fa91cba2f404f4cf0265c097e
Manish <patelmanish131149198@gmail.com>
2025-01-18 13:31:13 +0530

    Question_04

---
+++ EXTRAS_Submissions_on_Java_String_Practice_Problems/MostFrequent.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+public class MostFrequent {
+   
+	
+	public static int [] frequent(String str){
+		int[] temp={0,0};
+		for(int i=0;i<str.length();i++)
+		{
+			int count=0;
+		    char word='a';
+			
+			for(int j=0;j<str.length();j++)
+			{
+				word=str.charAt(i);
+				if(str.charAt(i)==str.charAt(j))
+				{
+					count++;
+				}
+				
+			}
+			
+			if(count>temp[0]){
+			temp[0]=count;
+			temp[1]=word;
+		
+		}}
+	return temp;
+	}
+	
+  
+	 public static void main(String[] args) {
+		
+	    Scanner input = new Scanner(System.in);
+        System.out.println("Enter  String :");
+		String str1= input.nextLine();
+		
+		int[] result= frequent(str1);
+		
+		System.out.println("Most Frequent Character: "+ (char)result[1] +" Count : "+result[0]);
+		
+        		
+		
+	 }
+}
+		
+		
+		
+
+
